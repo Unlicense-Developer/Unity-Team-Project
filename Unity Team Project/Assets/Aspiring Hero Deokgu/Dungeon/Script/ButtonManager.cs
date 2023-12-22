31ab6c8a772782f4d2f4b8c5636e949396e343c1
LSH777 <152233890+op3ggg@users.noreply.github.com>
2023-12-22 18:24:02 +0900

    12/22 18:22
    fixed up

--- Unity Team Project/Assets/Aspiring Hero Deokgu/Dungeon/Script/ButtonManager.cs
+++ Unity Team Project/Assets/Aspiring Hero Deokgu/Dungeon/Script/ButtonManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ButtonManager : MonoBehaviour
 {
 
     public static ButtonManager instance;
+
 
     private int activeButtons = 0;
 
@@ -17,7 +19,7 @@ public class ButtonManager : MonoBehaviour
         if (activeButtons >= 4)
         {
             BossActive.instance.DeviceActivated();
-            Debug.Log("당신은 어디선가 사악하고 탐욕스러운 기운을 느꼈다!");
+
         }
     }
 }
